400691e8ca076e1d4bc89035952f52905d12a3e0
welcomepalygame <welcomeplaygame@gmail.com>
2024-06-28 20:19:50 +0300

    28.06.2024

--- component/video/page-video.tsx
+++ component/video/page-video.tsx
@@ -2,28 +2,9 @@
 import React, { useEffect, useRef } from 'react';
 import classes from '../head/page-head.module.css';
 const VideoHead = () => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    const handleUnmute = () => {
-      if (videoRef.current) {
-        videoRef.current.muted = false;
-        videoRef.current.play().catch((error) => {
-          console.error('Error attempting to play video:', error);
-        });
-      }
-    };
-
-    document.addEventListener('click', handleUnmute);
-
-    return () => {
-      document.removeEventListener('click', handleUnmute);
-    };
-  }, []);
-
   return (
     <main className={`${classes.video_block}`}>
-      <video width={'100%'} autoPlay muted ref={videoRef}>
+      <video width={'100%'} autoPlay muted controls>
         <source src="/video/mevaro.mp4" type="video/mp4" />
       </video>
     </main>
