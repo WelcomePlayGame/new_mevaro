6aae96dc8ea5aae5a4aa3ce161547f72ef7d922a
welcomepalygame <welcomeplaygame@gmail.com>
2024-07-01 16:07:09 +0300

    01.07.2024_v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,21 @@ export default function RootLayout({
           gtag('js', new Date());
           gtag('config', 'G-CLY6EQ97JG');`}
         </Script>
+
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '398949992559159');
+          fbq('track', 'PageView');`}
+        </Script>
+        {/* End Meta Pixel Code */}
       </head>
       <body className={inter.className}>
         {/* Google Tag Manager (noscript) */}
@@ -51,6 +66,18 @@ export default function RootLayout({
             style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
         </noscript>
+
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=398949992559159&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel (noscript) */}
+
         {children}
       </body>
     </html>
