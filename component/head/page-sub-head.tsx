4efd209d8758c60923fcc9a0f9746f1863d4c09f
welcomepalygame <welcomeplaygame@gmail.com>
2024-07-01 17:34:03 +0300

    01.07.2024_v1

--- component/head/page-sub-head.tsx
+++ component/head/page-sub-head.tsx
@@ -1,42 +1,55 @@
-import Image from 'next/image';
+'use client';
 import clasess from './page-head.module.css';
 import TelegramSVG from '../svg/page-telegram';
 import ViberSVG from '../svg/page-viber';
-
+import { motion, AnimatePresence } from 'framer-motion';
+import { slideAnimation } from '@/lib/motion';
 const SubHead = () => {
   return (
     <main className={`${clasess.head_sub}`}>
-      <div className={`${clasess.container_logo_h1}`}>
-        <div className={`${clasess.container_logo}`}>
-          <img src={'/logo/logo.png'} alt="Mevaro" />
+      <AnimatePresence>
+        <div className={`${clasess.container_logo_h1}`}>
+          <motion.header
+            className={`${clasess.container_logo}`}
+            {...slideAnimation('left')}
+          >
+            <img src={'/logo/logo.png'} alt="Mevaro" />
+          </motion.header>
+
+          <motion.div {...slideAnimation('down')}>
+            <h1>Перетяжка меблів Київ</h1>
+          </motion.div>
         </div>
-        <div>
-          <h1>Перетяжка меблів Київ</h1>
-        </div>
-      </div>
-      <div className={`${clasess.container_logo_h1}`}>
-        <span>Телефонуйте:</span>
-        <a
-          href="tel:+380957162677?utm_source=website&utm_medium=link&utm_campaign=contact"
-          data-gtm="phone-call"
+        <motion.div
+          className={`${clasess.container_logo_h1}`}
+          {...slideAnimation('right')}
         >
-          📱 095 71 62 677
-        </a>
-      </div>
-      <div className={`${clasess.container_app}`}>
-        <a
-          href="https://t.me/romaniv21?utm_source=website&utm_medium=button&utm_campaign=contact"
-          data-gtm="telegram"
+          <span>Телефонуйте:</span>
+          <a
+            href="tel:+380957162677?utm_source=website&utm_medium=link&utm_campaign=contact"
+            data-gtm="phone-call"
+          >
+            📱 095 71 62 677
+          </a>
+        </motion.div>
+        <motion.div
+          className={`${clasess.container_app}`}
+          {...slideAnimation('right')}
         >
-          <TelegramSVG />
-        </a>
-        <a
-          href="viber://chat?number=%2B380957162677&utm_source=website&utm_medium=button&utm_campaign=contact"
-          data-gtm="viber"
-        >
-          <ViberSVG />
-        </a>
-      </div>
+          <a
+            href="https://t.me/romaniv21?utm_source=website&utm_medium=button&utm_campaign=contact"
+            data-gtm="telegram"
+          >
+            <TelegramSVG />
+          </a>
+          <a
+            href="viber://chat?number=%2B380957162677&utm_source=website&utm_medium=button&utm_campaign=contact"
+            data-gtm="viber"
+          >
+            <ViberSVG />
+          </a>
+        </motion.div>
+      </AnimatePresence>
     </main>
   );
 };
