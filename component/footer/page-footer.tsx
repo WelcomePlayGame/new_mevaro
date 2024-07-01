5e8988c90857edd6e0bfef7e844dc7bc8c9e1a71
welcomepalygame <welcomeplaygame@gmail.com>
2024-07-01 17:11:08 +0300

    01.07.2024_v1

--- component/footer/page-footer.tsx
+++ component/footer/page-footer.tsx
@@ -1,9 +1,33 @@
+import Link from 'next/link';
+import FacebookSVG from '../svg/page-facebook';
+import InstagramSVG from '../svg/page-instagram';
 import classes from './page-footer.module.css';
+
 const Footer = () => {
   return (
     <footer className={`${classes.footer}`}>
-      <div>Soc</div>
-      <div>Adress</div>
+      <div className={`${classes.container_soc}`}>
+        <div>
+          <Link
+            href={'https://www.facebook.com/www.mevaro.kiev.ua'}
+            target="_blank"
+          >
+            <FacebookSVG />
+          </Link>
+        </div>
+        <div>
+          <Link
+            href={'https://www.instagram.com/mevaro_peretyajka/'}
+            target="_blank"
+          >
+            <InstagramSVG />
+          </Link>
+        </div>
+      </div>
+      <div className={`${classes.container_adress}`}>
+        <span>місто Київ</span>
+        <span>вул. Екскаваторна 37</span>
+      </div>
     </footer>
   );
 };
