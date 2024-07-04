9e9ba139f034efad7d688a9db295ee9126c4d00e
welcomepalygame <welcomeplaygame@gmail.com>
2024-07-04 14:39:21 +0300

    04.07.2024_v1

---
+++ sitemap.ts
@@ -0,0 +1,16 @@
+export default async function sitemap() {
+  return [
+    {
+      url: `${process.env.BASE_URL}`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${process.env.BASE_URL}/office_armchair`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ];
+}
