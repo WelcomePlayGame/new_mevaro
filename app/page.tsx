a97630604a56eec753be68b82429c14eee328c75
welcomepalygame <welcomeplaygame@gmail.com>
2024-10-10 14:29:43 +0300

    10.10.2024_v1

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,10 @@ const firebaseConfig = {
   storageBucket: 'mevaro-48817.appspot.com',
   messagingSenderId: '999314351470',
   appId: '1:999314351470:web:2cd067d78ce33394504d7f',
-  measurementId: 'G-XE7MYY23CY',
+  vapidKey:
+    'BL6SI-ZFv4sJx8Q3hue4I9QvB20JLQ8FCO27EPH7aeW2NzEgnoHma8J2Q_OWb-WGB1lWDZtMfTwi3X8sYynTU-o',
 };
+
 export default function Home() {
   useEffect(() => {
     const app = initializeApp(firebaseConfig);
