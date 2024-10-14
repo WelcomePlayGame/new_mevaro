17027118c71625bcdb9bf793190c0391335fa536
welcomepalygame <welcomeplaygame@gmail.com>
2024-10-14 18:16:03 +0300

    mevaro_v1

--- lib/telegram_channel_bot.ts
+++ lib/telegram_channel_bot.ts
@@ -1,20 +1,21 @@
 import { Telegraf, Markup, Context } from 'telegraf';
-import {
-  Update,
-  ChatMember,
-  ChatMemberUpdated,
-} from 'telegraf/typings/core/types/typegram';
-import { mainMenuKeyboard } from './telegram_bot';
+import { ChatMemberUpdated } from 'telegraf/typings/core/types/typegram';
 const bot = new Telegraf(process.env.BOT_TOKEN as string);
 
-// Your channel username
-const CHANNEL_USERNAME = process.env.CHANNEL_USERNAME as string;
-
+// Channel username without "@"
+const CHANNEL_USERNAME = process.env.CHANNEL_USERNAME?.replace(
+  '@',
+  ''
+) as string;
+export const mainMenuKeyboard = Markup.keyboard([
+  ['🛠️ Перетяжка меблів', 'Заміна Пружинного Блоку'],
+  ['📞 Контакти', 'Про нас'],
+]).resize();
 // Function to check if a user is subscribed to the channel
 async function isSubscribed(ctx: Context, userId: number): Promise<boolean> {
   try {
     const chatMember = await ctx.telegram.getChatMember(
-      CHANNEL_USERNAME,
+      '@' + CHANNEL_USERNAME,
       userId
     );
     return ['member', 'administrator', 'creator'].includes(chatMember.status);
@@ -29,7 +30,7 @@ bot.on('chat_member', async (ctx) => {
 
   if (
     'username' in chatMemberUpdate.chat &&
-    chatMemberUpdate.chat.username === CHANNEL_USERNAME.slice(1) &&
+    chatMemberUpdate.chat.username === CHANNEL_USERNAME &&
     chatMemberUpdate.new_chat_member.status === 'member' &&
     chatMemberUpdate.old_chat_member.status !== 'member'
   ) {
@@ -39,11 +40,11 @@ bot.on('chat_member', async (ctx) => {
       // Send a welcome message to the user
       await ctx.telegram.sendMessage(
         userId,
-        `Вітаємо! Дякуємо за підписку на наш канал ${CHANNEL_USERNAME}. Як ми можемо вам допомогти?`,
-        mainMenuKeyboard
+        `Вітаємо! Дякуємо за підписку на наш канал @${CHANNEL_USERNAME}. Як ми можемо вам допомогти?`,
+        {
+          reply_markup: mainMenuKeyboard.reply_markup,
+        }
       );
-
-      // You can add more interactive elements or information here
     } catch (error) {
       console.error('Error sending welcome message:', error);
     }
