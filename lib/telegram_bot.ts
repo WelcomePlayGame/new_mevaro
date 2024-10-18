ba2837c7c0cbaddbe44359852403a7489463781c
welcomepalygame <welcomeplaygame@gmail.com>
2024-10-18 14:14:26 +0300

    mevaro_v1

--- lib/telegram_bot.ts
+++ lib/telegram_bot.ts
@@ -2,25 +2,51 @@ import { Telegraf, Markup } from 'telegraf';
 import { message } from 'telegraf/filters';
 const bot = new Telegraf(process.env.BOT_TOKEN as string);
 
-// Main menu keyboard without inline buttons
+// Main menu keyboard
 export const mainMenuKeyboard = Markup.keyboard([
   ['🛠️ Перетяжка меблів', 'Заміна Пружинного Блоку'],
   ['📞 Контакти', 'Про нас'],
 ]).resize();
 
-// Inline keyboard with a URL button for "Каталог Тканин"
 const mainMenuInline = Markup.inlineKeyboard([
-  Markup.button.url('🎨 Каталог Тканин', 'https://www.mevaro.kiev.ua/fabrics'),
+  [
+    Markup.button.url(
+      '🎨 Каталог Тканин',
+      'https://www.mevaro.kiev.ua/fabrics'
+    ),
+  ],
 ]);
 
 const returnMainMenuKeyboard = Markup.keyboard([
   ['🔙 Назад до головного меню'],
 ]).resize();
 
-// Submenu for "Перетяжка меблів"
 const reupholsterySubmenuKeyboard = Markup.keyboard([
   ['🛋️ Дивани', '🪑 Стільці'],
   ['🛏️ Ліжка', '🔙 Назад до головного меню'],
+]).resize();
+
+const reupholsterySubmenuforSofaKeyboard = Markup.keyboard([
+  ['🛋️ Прямий Диван', '🛋️↪️ Кутовий Диван'],
+  ['🔙 Назад до головного меню'],
+]).resize();
+
+const reupholsterySubmenuforChairKeyboard = Markup.keyboard([
+  ['🪑 Стілець зі спинкою', '🔲 Стілець без спинки'],
+  ['🔙 Назад до головного меню'],
+]).resize();
+
+const reupholsterySubmenuforBedKeyboard = Markup.keyboard([
+  ["🛌 Узголів'я ліжка", '🛏️ Ліжко'],
+  ['🔙 Назад до головного меню'],
+]).resize();
+
+const reupholsterySubmenuforSpringBlockKeyboard = Markup.keyboard([
+  [
+    'Заміна Пружинного Блоку без поролону',
+    'Заміна Пружинного Блоку з поролоном',
+  ],
+  ['🔙 Назад до головного меню'],
 ]).resize();
 
 // Start command
@@ -31,11 +57,9 @@ bot.command('start', async (ctx) => {
       caption: 'Ласкаво просимо до нашого бота компанії Mevaro!',
     });
 
-    // Then, send the main menu
+    // Then, send the menu options
     await ctx.reply('Що Вас цікавить?', mainMenuKeyboard);
-
-    // Then, send the inline button for "Каталог Тканин"
-    await ctx.reply('Оберіть розділ для переходу на сайт:', mainMenuInline);
+    await ctx.reply('Обрати тканину', mainMenuInline);
   } catch (error) {
     console.error('Error in start command:', error);
     await ctx.reply('Вибачте, сталася помилка. Спробуйте ще раз пізніше.');
@@ -53,7 +77,16 @@ bot.hears('🛠️ Перетяжка меблів', async (ctx) => {
 bot.hears('Заміна Пружинного Блоку', async (ctx) => {
   await ctx.reply(
     'Оберіть тип робіт з заміною пружинного блоку:',
-    reupholsterySubmenuKeyboard
+    reupholsterySubmenuforSpringBlockKeyboard
+  );
+});
+
+bot.hears('🎨 Каталог Тканин', async (ctx) => {
+  await ctx.reply(
+    'Переглянути наш каталог тканин можна на нашому сайті:',
+    Markup.inlineKeyboard([
+      Markup.button.url('Каталог тканин', 'https://www.mevaro.kiev.ua/fabrics'),
+    ])
   );
 });
 
@@ -70,14 +103,39 @@ bot.hears('Про нас', async (ctx) => {
   await ctx.reply('Інформація про нашу компанію...');
 });
 
+// Submenu options for "Перетяжка меблів"
+bot.hears('🛋️ Дивани', async (ctx) => {
+  await ctx.reply('Оберіть тип дивану:', reupholsterySubmenuforSofaKeyboard);
+});
+
+bot.hears('🪑 Стільці', async (ctx) => {
+  await ctx.reply('Оберіть тип стільця:', reupholsterySubmenuforChairKeyboard);
+});
+
+bot.hears('🛏️ Ліжка', async (ctx) => {
+  await ctx.reply('Оберіть тип ліжка:', reupholsterySubmenuforBedKeyboard);
+});
+
 bot.hears('🔙 Назад до головного меню', async (ctx) => {
   try {
     await ctx.reply('Ви повернулися до головного меню:', mainMenuKeyboard);
-    await ctx.reply('Оберіть розділ для переходу на сайт:', mainMenuInline);
   } catch (error) {
     console.error('Error in main menu handler:', error);
     await ctx.reply('Вибачте, сталася помилка. Спробуйте ще раз пізніше.');
   }
+});
+
+bot.hears('Заміна Пружинного Блоку без поролону', async (ctx) => {
+  await ctx.reply(
+    'Вартість заміни пружинного блоку без поролону: 4800 грн',
+    returnMainMenuKeyboard
+  );
+});
+bot.hears('Заміна Пружинного Блоку з поролоном', async (ctx) => {
+  await ctx.reply(
+    'Вартість заміни пружинного блоку з поролоном: 6000 грн +- грн в залежнотсі від щільності та висоти',
+    returnMainMenuKeyboard
+  );
 });
 
 // Catch-all handler for any text message
