e565e82271370ba84e5ef708f15861da20e7d44f
welcomepalygame <welcomeplaygame@gmail.com>
2024-10-18 14:07:34 +0300

    mevaro_v1

--- lib/telegram_bot.ts
+++ lib/telegram_bot.ts
@@ -2,51 +2,27 @@ import { Telegraf, Markup } from 'telegraf';
 import { message } from 'telegraf/filters';
 const bot = new Telegraf(process.env.BOT_TOKEN as string);
 
-// Main menu keyboard
+// Main menu keyboard with an inline button for the "Каталог Тканин"
 export const mainMenuKeyboard = Markup.keyboard([
   ['🛠️ Перетяжка меблів', 'Заміна Пружинного Блоку'],
   ['📞 Контакти', 'Про нас'],
-]).resize();
-
-const mainMenuInline = Markup.inlineKeyboard([
   [
     Markup.button.url(
       '🎨 Каталог Тканин',
       'https://www.mevaro.kiev.ua/fabrics'
     ),
-  ],
-]);
+  ], // Inline URL button
+]).resize();
 
+// Return to main menu button
 const returnMainMenuKeyboard = Markup.keyboard([
   ['🔙 Назад до головного меню'],
 ]).resize();
 
+// Submenu for "Перетяжка меблів"
 const reupholsterySubmenuKeyboard = Markup.keyboard([
   ['🛋️ Дивани', '🪑 Стільці'],
   ['🛏️ Ліжка', '🔙 Назад до головного меню'],
-]).resize();
-
-const reupholsterySubmenuforSofaKeyboard = Markup.keyboard([
-  ['🛋️ Прямий Диван', '🛋️↪️ Кутовий Диван'],
-  ['🔙 Назад до головного меню'],
-]).resize();
-
-const reupholsterySubmenuforChairKeyboard = Markup.keyboard([
-  ['🪑 Стілець зі спинкою', '🔲 Стілець без спинки'],
-  ['🔙 Назад до головного меню'],
-]).resize();
-
-const reupholsterySubmenuforBedKeyboard = Markup.keyboard([
-  ["🛌 Узголів'я ліжка", '🛏️ Ліжко'],
-  ['🔙 Назад до головного меню'],
-]).resize();
-
-const reupholsterySubmenuforSpringBlockKeyboard = Markup.keyboard([
-  [
-    'Заміна Пружинного Блоку без поролону',
-    'Заміна Пружинного Блоку з поролоном',
-  ],
-  ['🔙 Назад до головного меню'],
 ]).resize();
 
 // Start command
@@ -57,9 +33,8 @@ bot.command('start', async (ctx) => {
       caption: 'Ласкаво просимо до нашого бота компанії Mevaro!',
     });
 
-    // Then, send the menu options
+    // Then, send the main menu with inline and regular buttons
     await ctx.reply('Що Вас цікавить?', mainMenuKeyboard);
-    await ctx.reply('Обрати тканину', mainMenuInline);
   } catch (error) {
     console.error('Error in start command:', error);
     await ctx.reply('Вибачте, сталася помилка. Спробуйте ще раз пізніше.');
@@ -77,16 +52,7 @@ bot.hears('🛠️ Перетяжка меблів', async (ctx) => {
 bot.hears('Заміна Пружинного Блоку', async (ctx) => {
   await ctx.reply(
     'Оберіть тип робіт з заміною пружинного блоку:',
-    reupholsterySubmenuforSpringBlockKeyboard
-  );
-});
-
-bot.hears('🎨 Каталог Тканин', async (ctx) => {
-  await ctx.reply(
-    'Переглянути наш каталог тканин можна на нашому сайті:',
-    Markup.inlineKeyboard([
-      Markup.button.url('Каталог тканин', 'https://www.mevaro.kiev.ua/fabrics'),
-    ])
+    reupholsterySubmenuKeyboard
   );
 });
 
@@ -105,15 +71,15 @@ bot.hears('Про нас', async (ctx) => {
 
 // Submenu options for "Перетяжка меблів"
 bot.hears('🛋️ Дивани', async (ctx) => {
-  await ctx.reply('Оберіть тип дивану:', reupholsterySubmenuforSofaKeyboard);
+  await ctx.reply('Оберіть тип дивану:', reupholsterySubmenuKeyboard);
 });
 
 bot.hears('🪑 Стільці', async (ctx) => {
-  await ctx.reply('Оберіть тип стільця:', reupholsterySubmenuforChairKeyboard);
+  await ctx.reply('Оберіть тип стільця:', reupholsterySubmenuKeyboard);
 });
 
 bot.hears('🛏️ Ліжка', async (ctx) => {
-  await ctx.reply('Оберіть тип ліжка:', reupholsterySubmenuforBedKeyboard);
+  await ctx.reply('Оберіть тип ліжка:', reupholsterySubmenuKeyboard);
 });
 
 bot.hears('🔙 Назад до головного меню', async (ctx) => {
@@ -123,19 +89,6 @@ bot.hears('🔙 Назад до головного меню', async (ctx) => {
     console.error('Error in main menu handler:', error);
     await ctx.reply('Вибачте, сталася помилка. Спробуйте ще раз пізніше.');
   }
-});
-
-bot.hears('Заміна Пружинного Блоку без поролону', async (ctx) => {
-  await ctx.reply(
-    'Вартість заміни пружинного блоку без поролону: 4800 грн',
-    returnMainMenuKeyboard
-  );
-});
-bot.hears('Заміна Пружинного Блоку з поролоном', async (ctx) => {
-  await ctx.reply(
-    'Вартість заміни пружинного блоку з поролоном: 6000 грн +- грн в залежнотсі від щільності та висоти',
-    returnMainMenuKeyboard
-  );
 });
 
 // Catch-all handler for any text message
