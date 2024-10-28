f4b09b111514d1d7d7401cb665075d0af0c495c2
welcomepalygame <welcomeplaygame@gmail.com>
2024-10-28 18:34:38 +0200

    Mevaro_v2

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import Footer from '@/component/footer/page-footer';
+import Head from '@/component/head/page-head';
+import SubHead from '@/component/head/page-sub-head';
+
+const NotFound = () => {
+  return (
+    <>
+      <Head />
+      <SubHead />
+      <Footer />
+    </>
+  );
+};
+export default NotFound;
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,6 +7,33 @@ export default async function sitemap() {
     changeFrequency: 'yearly',
     priority: 1,
   }));
+  const modelVideos = fabrics
+    .filter((fabric) => fabric.url_video) // Оставляем только те записи, у которых есть видео
+    .map((fabric) => {
+      const youtubeId =
+        fabric.url_video.split('v=')[1] || fabric.url_video.split('/').pop();
+      const embedUrl = fabric.url_video.replace(
+        'youtube.com/shorts/',
+        'youtube.com/embed/'
+      );
+      const thumbnailUrl = `https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`;
+
+      return {
+        url: `${process.env.BASE_URL}/fabrics/${fabric.slug}`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.9,
+        video: {
+          title: fabric.title,
+          description: fabric.seo_content,
+          thumbnailUrl: thumbnailUrl,
+          contentUrl: `https://www.youtube.com/watch?v=${youtubeId}`, // Прямая ссылка на YouTube-видео
+          embedUrl: embedUrl, // Встраиваемый URL для видео
+          duration: fabric.duration || 60, // Длительность видео в секундах, если доступно
+          publicationDate: fabric.publicationDate || new Date().toISOString(), // Дата публикации
+        },
+      };
+    });
   return [
     {
       url: `${process.env.BASE_URL}`,
@@ -69,5 +96,6 @@ export default async function sitemap() {
       priority: 0.8,
     },
     ...modelFabrics,
+    ...modelVideos,
   ];
 }
