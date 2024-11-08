7d9a83dbe3048a9524c2003db570340d83dc81df
welcomepalygame <welcomeplaygame@gmail.com>
2024-11-08 22:50:30 +0200

    Mevaro_v2

--- component/video_section/page-video-section.tsx
+++ component/video_section/page-video-section.tsx
@@ -4,7 +4,13 @@ import classes from './page-video-section.module.css';
 const VideoSection = () => {
   return (
     <div className={`${classes.container_video_section}`}>
-      <video autoPlay muted loop className={`${classes.background_video}`}>
+      <video
+        poster="/image/mevaro.png"
+        autoPlay
+        muted
+        loop
+        className={`${classes.background_video}`}
+      >
         <source src="/video/mevaro.mp4" type="video/mp4" />
       </video>
     </div>
