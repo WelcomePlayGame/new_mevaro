af916cdbda704c15e5d72075a37613f54b23c8a0
welcomepalygame <welcomeplaygame@gmail.com>
2024-11-24 19:00:10 +0200

    Mevaro_v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -117,47 +117,59 @@ export default function RootLayout({
               '@context': 'https://schema.org',
               '@graph': [
                 {
-                  '@type': 'WebPage',
+                  '@type': 'LocalBusiness',
                   '@id': 'https://mevaro.kiev.ua',
+                  name: 'Mevaro',
                   url: 'https://mevaro.kiev.ua',
-                  name: metadata.title,
-                  review: {
-                    '@type': 'Review',
-                    reviewBody:
-                      'Робота сподобалася: якісно, професійно, відповідно до зазначеного терміну -рекомендую.',
-                    reviewRating: {
-                      '@type': 'Rating',
-                      ratingValue: '5',
-                      bestRating: '5',
-                    },
-                    author: {
-                      '@type': 'Person',
-                      name: 'Любов Петрович',
-                    },
-                    publisher: {
-                      '@type': 'Organization',
-                      name: 'Mevaro',
-                    },
-                    datePublished: '2024-11-24',
-                    url: 'https://g.page/r/CaVODvw2utWNEBM/review',
+                  address: {
+                    '@type': 'PostalAddress',
+                    addressLocality: 'Київ',
+                    addressCountry: 'UA',
                   },
-
-                  isPartOf: { '@id': 'https://mevaro.kiev.ua/' },
-                  datePublished: '2021-08-03T07:03:20+00:00',
-                  dateModified: new Date().toISOString(),
-                  description: metadata.description,
-                  breadcrumb: { '@id': 'https://mevaro.kiev.ua/' },
-                  inLanguage: 'uk',
-                  potentialAction: [
+                  aggregateRating: {
+                    '@type': 'AggregateRating',
+                    ratingValue: '5',
+                    reviewCount: '5',
+                  },
+                  review: [
                     {
-                      '@type': 'ReadAction',
-                      target: ['https://mevaro.kiev.ua/'],
+                      '@type': 'Review',
+                      reviewBody:
+                        'Робота сподобалася: якісно, професійно, відповідно до зазначеного терміну - рекомендую.',
+                      reviewRating: {
+                        '@type': 'Rating',
+                        ratingValue: '5',
+                        bestRating: '5',
+                      },
+                      author: {
+                        '@type': 'Person',
+                        name: 'Любов Петрович',
+                      },
+                      datePublished: '2024-11-24',
+                    },
+                    {
+                      '@type': 'Review',
+                      reviewBody:
+                        "Із великим задоволенням залишаю відгук про відмінну роботу майстрів Меваро🙌🏻 ✨ з мого старого і від початку не дуже доброго дивану вони зробили чудо! - новий, дуже гарний і зручний для відпочинку. Я мега задоволена! Фактично за пів ціни я отримала новий диван 👌🏻 повністю замінили наповнювач, підігнали всі нюанси по каркасу і замінили оббивку - він просто красень зараз 🤩 дуже кваліфіковані, чемні, професійні. Радять, пропонують великий вибір тканин. Вирішила, більш ніяких купівель меблів)) Дякую ще раз! За нагоди обов'язково буду звертатись 💙💛",
+
+                      reviewRating: {
+                        '@type': 'Rating',
+                        ratingValue: '5',
+                        bestRating: '5',
+                      },
+                      author: {
+                        '@type': 'Person',
+                        name: 'Ольга Ильчук',
+                      },
+                      datePublished: '2024-11-23',
                     },
                   ],
+                  description: metadata.description,
+                  image: metadata.openGraph.images[0]?.url,
                 },
                 {
                   '@type': 'BreadcrumbList',
-                  '@id': 'https://mevaro.kiev.ua/',
+                  '@id': 'https://mevaro.kiev.ua/breadcrumbs',
                   itemListElement: breadcrumbItems.map((item) => ({
                     '@type': 'ListItem',
                     position: item.position,
@@ -171,26 +183,17 @@ export default function RootLayout({
                   url: 'https://mevaro.kiev.ua/',
                   name: 'Mevaro',
                   inLanguage: 'uk',
-                  potentialAction: [
-                    {
-                      '@type': 'SearchAction',
-                      target: {
-                        '@type': 'EntryPoint',
-                        urlTemplate:
-                          'https://mevaro.kiev.ua/?s={search_term_string}',
-                      },
-                      'query-input': {
-                        '@type': 'PropertyValueSpecification',
-                        valueRequired: true,
-                        valueName: 'search_term_string',
-                      },
-                    },
-                  ],
+                  potentialAction: {
+                    '@type': 'SearchAction',
+                    target: 'https://mevaro.kiev.ua/?s={search_term_string}',
+                    'query-input': 'required name=search_term_string',
+                  },
                 },
               ],
             }),
           }}
         />
+
         <Script
           src="//web.webpushs.com/js/push/b6b24f64ee44d77c8800a47d8c4e1484_1.js"
           async
