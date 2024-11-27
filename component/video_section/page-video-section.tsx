773f12286614894a9f126392791faf6782f72954
welcomepalygame <welcomeplaygame@gmail.com>
2024-11-27 18:11:40 +0200

    Mevaro_v2

--- component/video_section/page-video-section.tsx
+++ component/video_section/page-video-section.tsx
@@ -8,20 +8,26 @@ const VideoSection = () => {
       itemType="http://schema.org/VideoObject"
       className={classes.container_video_section}
     >
+      <meta itemProp="name" content="Демонстрація відео компанії Mevaro" />
+      <meta itemProp="uploadDate" content="2024-11-27" />
       <meta itemProp="url" content="https://mevaro.kiev.ua" />
       <meta itemProp="thumbnailUrl" content="/image/mevaro.png" />
       <meta
         itemProp="contentUrl"
         content="https://mevaro.kiev.ua/video/mevaro.mp4"
       />
-
+      <meta
+        itemProp="description"
+        content="Демонстрація відео компанії Mevaro"
+      />
+      <meta itemProp="duration" content="PT35S" />{' '}
       <video
         poster="/image/mevaro.png"
         autoPlay
         muted
         loop
         className={classes.background_video}
-        aria-label="Демонстрация видео компании Mevaro"
+        aria-label="Демонстрація відео компанії Mevaro"
       >
         <source src="/video/mevaro.mp4" type="video/mp4" />
         Ваш браузер не поддерживает элемент <code>video</code>.
