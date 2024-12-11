fa2ba64d5ad3fdc124a0eaca81540ed8fc16c88c
welcomepalygame <welcomeplaygame@gmail.com>
2024-12-11 10:05:07 +0200

    Mevaro_v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -130,10 +130,6 @@ export default function RootLayout({
           }}
         />
 
-        <Script
-          src="//web.webpushs.com/js/push/b6b24f64ee44d77c8800a47d8c4e1484_1.js"
-          async
-        />
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5FMT447');`}
         </Script>
@@ -147,7 +143,7 @@ export default function RootLayout({
         <Script id="meta-pixel" strategy="afterInteractive">
           {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '398949992559159');fbq('track', 'PageView');`}
         </Script>
-        <Script async src="https://static.elfsight.com/platform/platform.js" />
+
         <Analytics />
       </head>
       <body className={inter.className}>
