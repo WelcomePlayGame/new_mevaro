a859ab1fff77571320f26d31232930d534dfb1ff
welcomepalygame <welcomeplaygame@gmail.com>
2025-01-28 09:27:11 +0200

    Mevaro_v2

--- app/robots.js
+++ app/robots.js
@@ -4,7 +4,7 @@ export default function robots() {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/admin/'],
+        disallow: ['/admin/', '/order'],
       },
       {
         userAgent: 'Yahoo',
