4f9f94d42c6874aa4153e3dd04eaba182ab1a833
WelcomePlayGame <welcomeplaygame@gmail.com>
2025-05-17 09:14:59 +0300

    Mevaro_v2

--- app/admin/blog/update/[slug]/page.tsx
+++ app/admin/blog/update/[slug]/page.tsx
@@ -85,7 +85,7 @@ const AddBlog = ({ params }: { params: { slug: string } }) => {
                 type="text"
                 placeholder="seo опис"
                 className="w-[250px] text-center border  rounded"
-                maxLength={150}
+                maxLength={160}
                 minLength={120}
                 name="seo_des"
                 required
